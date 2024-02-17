1d967bab599130bafb9c358b42e184c051f26494
Krish-Om <Krishombasukala@gmail.com>
2024-02-17 18:54:58 +0545

    Seed to plant 2

--- src/App.js
+++ src/App.js
@@ -394,7 +394,7 @@ class Home extends React.Component {
                   <div class="swiper-wrapper">
                     <a href="#" class="nav-link category-item swiper-slide">
                       <img src="assets/images/icon-vegetables-broccoli.png" alt="Category Thumbnail" />
-                      <h3 class="category-title">Seed</h3>
+                      <h3 class="category-title">Plant</h3>
                     </a>
                     <a href="#" class="nav-link category-item swiper-slide">
                       <img src="assets/images/icon-bread-baguette.png" alt="Category Thumbnail" />
