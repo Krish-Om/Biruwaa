8fa4b0086a57cafc9f8c1229f57c4e7debbf4bad
Krish-Om <Krishombasukala@gmail.com>
2024-02-22 16:46:03 +0545

    deleted newly-arived section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 class Home extends React.Component {
     
- 
+
   render() {
    
     return (
@@ -423,133 +423,6 @@ class Home extends React.Component {
             </div>
           </div>
         </section>
-
-
-        <section class="py-5 overflow-hidden">
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-md-12">
-
-                <div class="section-header d-flex flex-wrap flex-wrap justify-content-between mb-5">
-                  
-                  <h2 class="section-title">Newly Arrived Brands</h2>
-
-                  <div class="d-flex align-items-center">
-                    <a href="#" class="btn-link text-decoration-none">View All Categories →</a>
-                    <div class="swiper-buttons">
-                      <button class="swiper-prev brand-carousel-prev btn btn-yellow">❮</button>
-                      <button class="swiper-next brand-carousel-next btn btn-yellow">❯</button>
-                    </div>  
-                  </div>
-                </div>
-                
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12">
-
-                <div class="brand-carousel swiper">
-                  <div class="swiper-wrapper">
-                    
-                    <div class="swiper-slide">
-                      <div class="card mb-3 p-3 rounded-4 shadow border-0">
-                        <div class="row g-0">
-                          <div class="col-md-4">
-                            <img src="assets/images/product-thumb-11.jpg" class="img-fluid rounded" alt="Card title" />
-                          </div>
-                          <div class="col-md-8">
-                            <div class="card-body py-0">
-                              <p class="text-muted mb-0">Amber Jar</p>
-                              <h5 class="card-title">Honey best nectar you wish to get</h5>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="swiper-slide">
-                      <div class="card mb-3 p-3 rounded-4 shadow border-0">
-                        <div class="row g-0">
-                          <div class="col-md-4">
-                            <img src="assets/images/product-thumb-12.jpg" class="img-fluid rounded" alt="Card title" />
-                          </div>
-                          <div class="col-md-8">
-                            <div class="card-body py-0">
-                              <p class="text-muted mb-0">Amber Jar</p>
-                              <h5 class="card-title">Honey best nectar you wish to get</h5>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="swiper-slide">
-                      <div class="card mb-3 p-3 rounded-4 shadow border-0">
-                        <div class="row g-0">
-                          <div class="col-md-4">
-                            <img src="assets/images/product-thumb-13.jpg" class="img-fluid rounded" alt="Card title" />
-                          </div>
-                          <div class="col-md-8">
-                            <div class="card-body py-0">
-                              <p class="text-muted mb-0">Amber Jar</p>
-                              <h5 class="card-title">Honey best nectar you wish to get</h5>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="swiper-slide">
-                      <div class="card mb-3 p-3 rounded-4 shadow border-0">
-                        <div class="row g-0">
-                          <div class="col-md-4">
-                            <img src="assets/images/product-thumb-14.jpg" class="img-fluid rounded" alt="Card title" />
-                          </div>
-                          <div class="col-md-8">
-                            <div class="card-body py-0">
-                              <p class="text-muted mb-0">Amber Jar</p>
-                              <h5 class="card-title">Honey best nectar you wish to get</h5>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="swiper-slide">
-                      <div class="card mb-3 p-3 rounded-4 shadow border-0">
-                        <div class="row g-0">
-                          <div class="col-md-4">
-                            <img src="assets/images/product-thumb-11.jpg" class="img-fluid rounded" alt="Card title" />
-                          </div>
-                          <div class="col-md-8">
-                            <div class="card-body py-0">
-                              <p class="text-muted mb-0">Amber Jar</p>
-                              <h5 class="card-title">Honey best nectar you wish to get</h5>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="swiper-slide">
-                      <div class="card mb-3 p-3 rounded-4 shadow border-0">
-                        <div class="row g-0">
-                          <div class="col-md-4">
-                            <img src="assets/images/product-thumb-12.jpg" class="img-fluid rounded" alt="Card title" />
-                          </div>
-                          <div class="col-md-8">
-                            <div class="card-body py-0">
-                              <p class="text-muted mb-0">Amber Jar</p>
-                              <h5 class="card-title">Honey best nectar you wish to get</h5>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    
-                  </div>
-                </div>
-
-              </div>
-            </div>
-          </div>
-        </section>
-
 
         <section class="py-5">
           <div class="container-fluid">
